e876f918e353548bd424c7769b45b04e7eef9655
erikasbulev <erikas.bulevicius@gmail.com>
2016-03-07 21:46:34 +0200

    Initial commit

---
+++ app/src/main/java/lt/ktu/tester/app/MainActivity.java
@@ -0,0 +1,15 @@
+package lt.ktu.tester.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by erikas on 07/03/16.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
